0d647da3c95c49649c67c4302c1d6c246fdb1be7
Tina-Lightning <c.atherholt@gmail.com>
2021-01-28 20:48:39 -0500

    got navbar working

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Navbar from "./components/navbar.component";
+import MoviesList from "./components/movies-list.component";
+import EditMovie from "./components/edit-movie.component";
+import CreateMovie from "./components/create-movie.component";
+import CreateUser from "./components/create-user.component";
+
 function App() {
-  return <div className="container">hello</div>;
+  return (
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={MoviesList} />
+      <Route path="/edit/:id" component={EditMovie} />
+      <Route path="/create" component={CreateMovie} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
+  );
 }
 
 export default App;

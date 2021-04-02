2a1e0f1625ebf954cc6e73c97320fcdbea497a56
Tina-Lightning <c.atherholt@gmail.com>
2021-04-01 20:52:55 -0400

    got fetch working

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { FaTv } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>This will be a navbar</h1>
-    </div>
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+      <Link to="/" className="navbar-brand">
+        <FaTv /> TV Logging App
+      </Link>
+      <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">
+              Link 1
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">
+              Link 2
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">
+              Link 3
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
--- client/src/context.js
+++ client/src/context.js
@@ -1,12 +1,44 @@
 import React, { useState, useContext, useEffect } from "react";
 
-export const API_ENDPOINT = `https://api.themoviedb.org/3/search/tv?api_key=${process.env.REACT_APP_TVSHOW_API_KEY}&query=seinfeld`;
-console.log(API_ENDPOINT);
+export const API_ENDPOINT = `https://api.themoviedb.org/3/search/tv?api_key=${process.env.REACT_APP_TVSHOW_API_KEY}`;
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const [shows, setShows] = useState([]);
+  const [query, setQuery] = useState("friend");
+
+  const fetchShows = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      console.log("Name of show: " + data.results[0].name);
+
+      if (data.results.total_results > 0) {
+        setShows(data.results);
+        setError({ show: false, msg: "" });
+      } else {
+        setError({ show: true, msg: "Show not found" });
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchShows(`${API_ENDPOINT}&query=${query}`);
+  }, [query]);
+
+  return (
+    <AppContext.Provider value={{ isLoading, error, shows, query, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
